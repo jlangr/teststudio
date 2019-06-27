20fd187c4495eea16d04517215832fcb00db9db3
Jeff Langr <jeff.langr@sonicdrivein.com>
2019-06-27 14:00:01 -0700

    working submit

--- app/src/App.js
+++ app/src/App.js
@@ -1,20 +1,36 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 import './App.css'
 
 export default class App extends Component {
+  constructor() {
+    super()
+    this.state = { message: '', selectedFile: null }
+  }
+
+  submit() {
+    //this.setState({ message: '' })
+    const data = new FormData()
+    data.append('fileToUpload', this.state.selectedFile)
+    axios.post("http://localhost:3001/upload", data, {})
+      .then(response => this.setState({ message: 'file uploaded' }))
+      .catch(response => this.setState({ message: 'File too large' }))
+  }
+
+  changeFile(event) {
+    this.setState({ selectedFile: event.target.files[0], loaded: 0 })
+  }
+
   render() {
     return (
       <div>
         <p>Upload a file to the server:</p>
-        <form ref='uploadForm'
-              id='uploadForm'
-              action='http://localhost:3001/upload'
-              method='post'
-              encType='multipart/form-data'>
-          <input type='file' name='fileToUpload' />
-          <input type='submit' value='Upload!' />
-        </form>
+          <input type='file' name='fileToUpload' onChange={this.changeFile.bind(this)} />
+          <button onClick={this.submit.bind(this)}>Upload!</button>
+        <p></p>
+        <p>{this.state.message}</p>
       </div>
     )
   }
 }
+
--- server/index.js
+++ server/index.js
@@ -2,21 +2,38 @@ const express = require('express')
 const fileUpload = require('express-fileupload')
 const app = express()
 
+const allowCORS = app => {
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept')
+    next()
+  })
+}
+allowCORS(app)
+
 app.use(fileUpload())
 
 app.post('/upload', (request, response) => {
-  if (Object.keys(request.files).length == 0)
-    return response.status(400).send('No files were uploaded.')
-
   const file = request.files.fileToUpload
-  if (file.size >= 32768)
+  if (file.size >= 32768) {
+    console.log('file too large; returning 400')
+//    response.statusCode = 400
+//    response.write('File too large')
+//    return response.end()
+
+//    response.statusMessage = "File too large"
+//    response.status(400).end()
+
     return response.status(400).send('File too large')
+  }
 
   file.mv(`./upload${file.name}`, err => {
     if (err) {
-      response.status = 500
-      response.send('unable to move file')
-      response.end()
+      console.log('unable to move; returning 400')
+      response.statusCode = 500
+      response.write('unable to move file')
+      return response.end()
     }
     else
       return response.send('file uploaded')
