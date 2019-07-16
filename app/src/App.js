9f67042a8d4e0c97ef9b7ddc75b4f41a7c2dcebf
Jeff Langr <jeff.langr@sonicdrivein.com>
2019-07-16 16:14:17 -0700

    onkeyup

--- app/src/App.js
+++ app/src/App.js
@@ -21,13 +21,28 @@ export default class App extends Component {
     this.setState({ selectedFile: event.target.files[0], loaded: 0 })
   }
 
+  validate(event) {
+    const email = event.target.value
+console.log('email: ', email)
+    const emailRegex = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/
+
+    if (!emailRegex.test(email))
+      this.setState({ message: 'invalid email' })
+    else
+      this.setState({ message: '' })
+  }
+
   render() {
     return (
       <div>
         <p>Upload a file to the server:</p>
           <input type='file' name='fileToUpload' onChange={this.changeFile.bind(this)} />
           <button onClick={this.submit.bind(this)}>Upload!</button>
+
+        <p>Your email: <input type='text' id='email' onKeyUp={this.validate.bind(this)} /></p>
+
         <p></p>
+
         <p>{this.state.message}</p>
       </div>
     )
