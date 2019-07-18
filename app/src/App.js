02d4ad62fce1255eafc48c279f468e7fa6f6958d
Jeff Langr <jeff.langr@sonicdrivein.com>
2019-07-18 16:48:15 -0700

    added library stuff

--- app/src/App.js
+++ app/src/App.js
@@ -6,7 +6,10 @@ import './App.css'
 export default class App extends Component {
   constructor() {
     super()
-    this.state = { message: '', selectedFile: null }
+    this.state = { 
+      message: '', 
+      selectedFile: null, 
+      dueDate: '' }
   }
 
   submit() {
@@ -33,12 +36,36 @@ export default class App extends Component {
       this.setState({ message: '' })
   }
 
+  checkout(event) {
+    const due = this.addDays(new Date(), 21)
+    this.setState({ dueDate: this.formatDate(due) })
+  }
+
+  formatDate(date=new Date(), separator='/') {
+    let dd = date.getDate()
+    let mm = date.getMonth() + 1
+    const yyyy = date.getFullYear()
+
+    if (dd < 10) dd='0'+dd
+    if (mm < 10) mm='0'+mm
+
+    return mm + separator + dd + separator + yyyy
+ }
+
+  addDays(fromDate, days) {
+    var date = new Date(fromDate.valueOf())
+    date.setDate(fromDate.getDate() + days);
+    return date;
+  }
+
   render() {
     return (
       <div>
         <hr />
+           <h3>Email Validator</h3>
            <p>Your email: <input type='text' id='email' onKeyUp={this.validate.bind(this)} /></p>
         <hr />
+           <h3>File Uploader</h3>
             <p>Upload a file to the server:</p>
             <input type='file' name='fileToUpload' onChange={this.changeFile.bind(this)} />
             <button onClick={this.submit.bind(this)}>Upload!</button>
@@ -47,6 +74,14 @@ export default class App extends Component {
             <p></p>
 
             <p><label id='errorMessage'>{this.state.message}</label></p>
+
+        <hr />
+        <hr />
+            <h3>Library System</h3>
+            <p>today is {this.formatDate()}</p>
+            <p>your book is due ... {this.state.dueDate}</p>
+            <button onClick={this.checkout.bind(this)}>Check Out book</button>
+            <p></p>
       </div>
     )
   }
