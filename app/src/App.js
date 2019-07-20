98a5d4b3500fdb8a1a743f073690274aa4fa5c64
Jeff Langr <jeff.langr@sonicdrivein.com>
2019-07-20 08:41:35 -0700

    ok

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,3 @@
-import Promise from 'es6-promise'
 import React, { Component } from 'react'
 import axios from 'axios'
 import './App.css'
@@ -9,7 +8,10 @@ export default class App extends Component {
     this.state = { 
       message: '', 
       selectedFile: null, 
-      dueDate: '' }
+      dueDate: '',
+      findme1: '',
+    }
+    this.findme1Click.bind(this)
   }
 
   submit() {
@@ -38,18 +40,17 @@ export default class App extends Component {
 
   checkout(event) {
     const due = this.addDays(new Date(), 21)
-    this.setState({ dueDate: this.formatDate(due) })
+    this.setState({ dueDate: this.formatDate(due) + " " })
   }
 
   formatDate(date=new Date(), separator='/') {
-    let dd = date.getDate()
-    let mm = date.getMonth() + 1
+    const dd = date.getDate()
+    const mm = date.getMonth() + 1
     const yyyy = date.getFullYear()
 
-    if (dd < 10) dd='0'+dd
-    if (mm < 10) mm='0'+mm
-
-    return mm + separator + dd + separator + yyyy
+    const dayOfWeek = date.getDay()
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    return daysOfWeek[dayOfWeek] + ' ' + mm + separator + dd + separator + yyyy
  }
 
   addDays(fromDate, days) {
@@ -58,30 +59,65 @@ export default class App extends Component {
     return date;
   }
 
+  findme1Click(message1) {
+    this.setState({ findme1: message1 })
+  }
+
   render() {
     return (
       <div>
-        <hr />
-           <h3>Email Validator</h3>
-           <p>Your email: <input type='text' id='email' onKeyUp={this.validate.bind(this)} /></p>
-        <hr />
-           <h3>File Uploader</h3>
-            <p>Upload a file to the server:</p>
-            <input type='file' name='fileToUpload' onChange={this.changeFile.bind(this)} />
-            <button onClick={this.submit.bind(this)}>Upload!</button>
+        <div>
+          <hr />
+             <h3>Email Validator</h3>
+             <p>Your email: <input type='text' id='email' onKeyUp={this.validate.bind(this)} /></p>
 
+          <hr />
+             <h3>File Uploader</h3>
+              <p>Upload a file to the server:</p>
+              <input type='file' name='fileToUpload' onChange={this.changeFile.bind(this)} />
+              <button onClick={this.submit.bind(this)}>Upload!</button>
+              <p></p>
+              <p><label id='errorMessage'>{this.state.message}</label></p>
 
-            <p></p>
+          <hr />
+              <h3>Library System</h3>
+              <p>today is {this.formatDate()}</p>
+              <p>your book is due: </p>
+              <div id="dueDate"><p>{this.state.dueDate}</p></div>
+              <button onClick={this.checkout.bind(this)}>Check Out book</button>
+        </div>
 
-            <p><label id='errorMessage'>{this.state.message}</label></p>
+        <div id="duplicateId">
+          <hr />
+          <h3>Oh no, duplicate ID</h3>
+          <div id="patientName">
+            <h4>Patient Name</h4>
+            <input type="text" />
+            <button onClick={e => this.findme1Click("sorry, maybe next time")}>Submit</button><br />
+          </div>
+          <div id="operatingRoomName">
+            <h4>Operating Room Name</h4>
+            <input type="text" />
+            <button onClick={e => this.findme1Click("you have chosen wisely!")}>Submit</button>
+          </div>
+          <p><label>{this.state.findme1}</label></p>
+          <hr />
+          <hr />
+        </div>
 
-        <hr />
-        <hr />
-            <h3>Library System</h3>
-            <p>today is {this.formatDate()}</p>
-            <p>your book is due ... {this.state.dueDate}</p>
-            <button onClick={this.checkout.bind(this)}>Check Out book</button>
-            <p></p>
+        <div id="">
+        <h3></h3>
+        </div>
+
+        <h3>Disclaimers</h3>
+        <p>Ipsum dorum ...</p>
+        <p>Ipsum dorum ...</p>
+        <p>Ipsum dorum ...</p>
+        <p>Ipsum dorum ...</p>
+        <p>Ipsum dorum ...</p>
+        <p>Ipsum dorum ...</p>
+        <p>Ipsum dorum ...</p>
+        <p>Ipsum dorum ...</p>
       </div>
     )
   }
