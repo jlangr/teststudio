0a670d99e5ceb33ae243a61334ecc26c48336c50
Jeff Langr <Jeff@Langrsoft.com>
2019-07-20 10:40:23 -0600

    sort table

--- app/src/EditTable.js
+++ app/src/EditTable.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
 import ContentEditable from 'react-contenteditable'
 import { Table, Button } from 'semantic-ui-react'
 import './styles.css'
@@ -7,14 +6,14 @@ import './styles.css'
 export default class EditTable extends Component {
   initialState = {
     store: [
-      { id: 1, item: 'silver', price: 15.41 },
-      { id: 2, item: 'gold', price: 1284.3 },
-      { id: 3, item: 'platinum', price: 834.9 },
-    ],
+      { id: 1, name: 'aspirin (ea.)', price: 7.29 },
+      { id: 2, name: 'fancy bandage', price: 49.50 },
+      { id: 3, name: 'blood bucket', price: 99.98 },
+      { id: 4, name: 'regular bandage', price: 12.20 }],
     row: {
-      item: '',
+      name: '',
       price: '',
-    },
+    }
   }
 
   state = this.initialState
@@ -29,10 +28,7 @@ export default class EditTable extends Component {
         .replace(/&gt;/g, '>')
         .replace(/&lt;/g, '<')
     }
-    const trimmedRow = {
-      ...row,
-      item: trimSpaces(row.item),
-    }
+    const trimmedRow = { ...row, name: trimSpaces(row.name) }
 
     row.id = store.length + 1
 
@@ -47,14 +43,11 @@ export default class EditTable extends Component {
   deleteRow = id => {
     const { store } = this.state
 
-    this.setState({
-      store: store.filter(item => id !== item.id),
-    })
+    this.setState({ store: store.filter(item => id !== item.id) })
   }
 
   disableNewlines = event => {
     const keyCode = event.keyCode || event.which
-
     if (keyCode === 13) {
       event.returnValue = false
       if (event.preventDefault) event.preventDefault()
@@ -74,7 +67,6 @@ export default class EditTable extends Component {
 
   pasteAsPlainText = event => {
     event.preventDefault()
-
     const text = event.clipboardData.getData('text/plain')
     document.execCommand('insertHTML', false, text)
   }
@@ -86,11 +78,10 @@ export default class EditTable extends Component {
   }
 
   handleContentEditable = event => {
+    console.log("BBB")
     const { row } = this.state
     const {
-      currentTarget: {
-        dataset: { column },
-      },
+      currentTarget: { dataset: { column }, },
       target: { value },
     } = event
 
@@ -98,6 +89,7 @@ export default class EditTable extends Component {
   }
 
   handleContentEditableUpdate = event => {
+    console.log("AAA")
     const { store } = this.state
 
     const {
@@ -110,102 +102,77 @@ export default class EditTable extends Component {
     let updatedRow = store.filter((item, i) => parseInt(i) === parseInt(row))[0]
     updatedRow[column] = value
 
-    this.setState({
-      store: store.map((item, i) => (item[column] === row ? updatedRow : item)),
-    })
+    this.setState({ store: store.map(item => (item[column] === row ? updatedRow : item)), })
   }
 
   render() {
+    console.log('RENDER')
     const {
       store,
-      row: { item, price },
+      row: { name, price },
     } = this.state
+
+    const s = store.sort((itemA, itemB) => itemA.name < itemB.name ? -1 : 1);
+    console.log(`sorted:`)
+    store.forEach(r => {
+      console.log(`r: ${r.name}`)
+    })
 
     return (
       <div className="App">
-      <h1>React Contenteditable</h1>
-
-    <Table celled>
-    <Table.Header>
-    <Table.Row>
-    <Table.HeaderCell>Item</Table.HeaderCell>
-    <Table.HeaderCell>Price</Table.HeaderCell>
-    <Table.HeaderCell>Action</Table.HeaderCell>
-    </Table.Row>
-    </Table.Header>
-    <Table.Body>
-    {store.map((row, i) => {
-        return (
-          <Table.Row key={row.id}>
-          <Table.Cell className="narrow">
-          <ContentEditable
-        html={row.item}
-        data-column="item"
-        data-row={i}
-        className="content-editable"
-        onKeyPress={this.disableNewlines}
-        onPaste={this.pasteAsPlainText}
-        onFocus={this.highlightAll}
-        onChange={this.handleContentEditableUpdate}
-        />
-        </Table.Cell>
-        <Table.Cell className="narrow">
-          <ContentEditable
-        html={row.price.toString()}
-        data-column="price"
-        data-row={i}
-        className="content-editable"
-        onKeyPress={this.validateNumber}
-        onPaste={this.pasteAsPlainText}
-        onFocus={this.highlightAll}
-        onChange={this.handleContentEditableUpdate}
-        />
-        </Table.Cell>
-        <Table.Cell className="narrow">
-          <Button
-        onClick={() => {
-          this.deleteRow(row.id)
-        }}
-      >
-        Delete
-        </Button>
-        </Table.Cell>
-        </Table.Row>
-      )
-      })}
-    <Table.Row>
-    <Table.Cell className="narrow">
-      <ContentEditable
-    html={item}
-    data-column="item"
-    className="content-editable"
-    innerRef={this.firstEditable}
-    onKeyPress={this.disableNewlines}
-    onPaste={this.pasteAsPlainText}
-    onFocus={this.highlightAll}
-    onChange={this.handleContentEditable}
-    />
-    </Table.Cell>
-    <Table.Cell className="narrow">
-      <ContentEditable
-    html={price}
-    data-column="price"
-    className="content-editable"
-    onKeyPress={this.validateNumber}
-    onPaste={this.pasteAsPlainText}
-    onFocus={this.highlightAll}
-    onChange={this.handleContentEditable}
-    />
-    </Table.Cell>
-    <Table.Cell className="narrow">
-      <Button disabled={!item || !price} onClick={this.addRow}>
-      Add
-      </Button>
-      </Table.Cell>
-      </Table.Row>
-      </Table.Body>
+      <h1>Schedule of Items</h1>
+      <Table celled>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Name</Table.HeaderCell>
+            <Table.HeaderCell>Price</Table.HeaderCell>
+            <Table.HeaderCell>Action</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {
+            s.map((row, i) => {
+            return (
+              <Table.Row key={row.id}>
+              <Table.Cell className="narrow">
+              <ContentEditable
+                html={row.name}
+                data-column="name" data-row={i} className="content-editable"
+                onKeyPress={this.disableNewlines} onPaste={this.pasteAsPlainText} onFocus={this.highlightAll} onChange={this.handleContentEditableUpdate}
+              />
+            </Table.Cell>
+            <Table.Cell className="narrow">
+              <ContentEditable html={row.price.toString()}
+                data-column="price" data-row={i} className="content-editable"
+                onKeyPress={this.validateNumber} onPaste={this.pasteAsPlainText} onFocus={this.highlightAll} onChange={this.handleContentEditableUpdate}
+              />
+            </Table.Cell>
+            <Table.Cell className="narrow">
+              <Button onClick={() => { this.deleteRow(row.id) }} > Delete </Button>
+            </Table.Cell>
+            </Table.Row>
+          )
+          })}
+          <Table.Row>
+            <Table.Cell className="narrow">
+              <ContentEditable html={name}
+                data-column="name" className="content-editable" innerRef={this.firstEditable}
+                onKeyPress={this.disableNewlines} onPaste={this.pasteAsPlainText} onFocus={this.highlightAll} onChange={this.handleContentEditable}
+              />
+            </Table.Cell>
+            <Table.Cell className="narrow">
+              <ContentEditable html={price}
+                data-column="price" className="content-editable"
+                onKeyPress={this.validateNumber} onPaste={this.pasteAsPlainText} onFocus={this.highlightAll} onChange={this.handleContentEditable}
+              />
+            </Table.Cell>
+            <Table.Cell className="narrow">
+              <Button disabled={!name || !price} onClick={this.addRow}>Add</Button>
+            </Table.Cell>
+          </Table.Row>
+        </Table.Body>
       </Table>
-      </div>
+    </div>
   )
   }
 }
